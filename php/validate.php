40b55403dc29e04ac6fe3144aec1017208dc567c
yuvraj46 <yuvraj@udel.edu>
2014-03-09 22:26:34 -0700

    Update validate.php

--- php/validate.php
+++ php/validate.php
@@ -8,7 +8,8 @@
   }
   // Query database to check if the username is available
   $query = "Select * from  where username = '$user' ";
-  // Execute the above query using your own script and if it return you the // result (row) we should return negative, else a success message.
+  // Execute the above query using your own script and if it return you the 
+  // result (row) we should return negative, else a success message.
   $available = true;
   if($available) {
     echo json_encode(array('code' => 1,
