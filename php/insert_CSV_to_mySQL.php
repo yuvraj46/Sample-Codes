f48d977f331ecdc8e920c1078467b7fe13e8f622
yuvraj46 <yuvraj@udel.edu>
2014-03-09 22:33:23 -0700

    Create insert_CSV_to_mySQL.php

---
+++ php/insert_CSV_to_mySQL.php
@@ -0,0 +1,30 @@
+<?php
+//connect to the database
+$connect = mysql_connect(""); mysql_select_db("",$connect); //select the table //
+if ($_FILES[csv][size] > 0) {
+//get the csv file
+$file = $_FILES[csv][tmp_name]; $handle = fopen($file,"r");
+//loop through the csv file and insert into database do {
+if ($data[0]) {
+mysql_query("INSERT INTO cip (cip, cipname) VALUES
+(
+'".addslashes($data[0])."', '".addslashes($data[1])."'
+) ");
+}
+} while ($data = fgetcsv($handle,1000,";","'"));
+//redirect
+header('Location: import.php?success=1'); die;
+} ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> <title>Import a CSV File with PHP & MySQL</title>
+</head>
+<body>
+<?php if (!empty($_GET[success])) { echo "<b>Your file has been imported.</b><br><br>"; } //generic success notice ?>
+<form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
+Yuvraj Khadke
+Choose your file: <br />
+<input name="csv" type="file" id="csv" />
+<input type="submit" name="Submit" value="Submit" />
+</form>
+</body> </html>
