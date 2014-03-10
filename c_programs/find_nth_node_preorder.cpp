e3f6781c4cb1e37054663a9d16b6349b080a0d8d
yuvraj46 <yuvraj@udel.edu>
2014-03-09 23:24:32 -0700

    Create find_nth_node_preorder.cpp

---
+++ c_programs/find_nth_node_preorder.cpp
@@ -0,0 +1,29 @@
+/*
+findNthNode
+
+params:
+- pointer to the root of a binary tree
+- integer n
+
+return:
+- pointer to the nth node, inorder traversal
+
+
+     A
+   B   C
+ D    E  F
+ 
+*/
+
+Node *find(int *root, int *toFind)
+{
+    if(*toFind==0 || root!= NULL)
+        return(NULL);
+    if(root)
+    {
+        find(root->left,&(*toFind-1));
+        find(root->right,&(*toFind-1));
+    }
+    return(root->value);
+
+}
