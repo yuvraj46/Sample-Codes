0273b77b406d8488a53b5e4aeb3ca749474a6161
yuvraj46 <yuvraj@udel.edu>
2014-03-09 22:39:53 -0700

    Update autoComplete.php

--- php/autoComplete.php
+++ php/autoComplete.php
@@ -7,13 +7,19 @@
   $result = mysql_query("");
   while ($row = mysql_fetch_assoc($result)) {
     $colors[]=$row['cip'];
-  } mysql_free_result($result); mysql_close($link);
-  // check the parameter if(isset($_GET['part']) and $_GET['part'] != '') {
-  // initialize the results array $results = array();
-  // search colors foreach($colors as $color) {
-  // if it starts with 'part' add to results if( strpos($color, $_GET['part']) === 0 ){
-  $results[] = $color;
-  } }
+  }
+  mysql_free_result($result); mysql_close($link);
+  // check the parameter 
+  if(isset($_GET['part']) and $_GET['part'] != '') {
+    // initialize the results array
+    $results = array();
+    // search colors 
+    foreach($colors as $color) {
+    // if it starts with 'part' add to results 
+     if( strpos($color, $_GET['part']) === 0 ){
+        $results[] = $color;
+      }
+    }
   // return the array as json with PHP 5.2 echo json_encode($results);
   }
 ?>
